ce6f9c960a3b9d864d016d1a4b12356318e0f740
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-08-15 23:30:42 +0900

    feat: Add process to main.ts.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,40 @@
-function run() {}
+import { getInputParameter, outputAndCommitMarkdown } from "./lib/action";
+import { getPrs } from "./lib/github";
+import { groupPRsByRepository } from "./lib/grouping";
+import { generateFullMarkdown } from "./lib/markdown";
+import { Options, Status } from "./types";
+
+async function run() {
+  // Get input parameters from action inputs.
+  const {
+    userName,
+    isExcludeOwnerRepos,
+    excludedRepos,
+    states,
+    repoTemplate,
+    itemTemplate,
+    outputFilePath,
+  } = getInputParameter();
+
+  // Prepare options for fetching PRs.
+  const options: Options = {
+    userName,
+    isExcludeOwnerRepos,
+    excludedRepos,
+    states: states as Status[],
+  };
+
+  // Fetch PRs based on the provided options.
+  const prs = await getPrs(options);
+
+  // Group fetched PRs by repository.
+  const groupedPrs = groupPRsByRepository(prs);
+
+  // Generate markdown content based on the grouped PRs.
+  const mdData = generateFullMarkdown(groupedPrs, repoTemplate, itemTemplate);
+
+  // Write markdown content to a file and commit it using Git.
+  outputAndCommitMarkdown(outputFilePath, mdData);
+}
 
 void run();
