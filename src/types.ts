663f357695b30e4fe9dfcab2e924a71b6ad0dcd7
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-08-15 17:18:05 +0900

    feat: Define Status and Options types
    - Define Status type to represent PR statuses (open, merged, closed)
    - Define Options type to customize the PR query with various parameters

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+// Define the possible status values for Pull Requests
+export type Status = "open" | "merged" | "closed";
+
+// Define the options that can be used to customize the PR query
+export type Options = {
+  userName: string; // The username of the PR author
+  isExcludeOwnerRepos?: boolean; // Whether to exclude PRs from repositories owned by the user
+  excludedRepos?: string[]; // List of repositories to exclude from the query
+  states?: Status[]; // List of PR states (open, merged, closed) to include in the query
+};
